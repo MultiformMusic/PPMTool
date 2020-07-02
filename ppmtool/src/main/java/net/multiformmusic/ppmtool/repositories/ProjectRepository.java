5ea7cd6967154a00beb47650abecf3f16a383b3b
mdio <michel.dio33@gmail.com>
2020-07-02 23:40:49 +0200

    Project repo + service + controller

---
+++ ppmtool/src/main/java/net/multiformmusic/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,11 @@
+package net.multiformmusic.ppmtool.repositories;
+
+import net.multiformmusic.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+}
+
---
+++ ppmtool/src/main/java/net/multiformmusic/ppmtool/services/ProjectService.java
@@ -0,0 +1,17 @@
+package net.multiformmusic.ppmtool.services;
+
+import net.multiformmusic.ppmtool.domain.Project;
+import net.multiformmusic.ppmtool.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public Project saveOrUpdateProject(Project project) {
+        return projectRepository.save(project);
+    }
+}
